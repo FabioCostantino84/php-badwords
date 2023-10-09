cba84555c839223662446e81965648bafe722a44
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-09 15:58:11 +0200

    add form + imput

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+
+<form>
+    <input type="text" name="frase" id="frase" placeholder="scrivi una frase">
+    <input type="text" name="censured" id="censured" placeholder="parola da censurare">
+    <button type="submit">Save</button>
+</form>
+
+<h2>La tua frase: <?php echo $_GET['frase']?></h2>
+<h2>Parola censurata: <?php echo $_GET['censured']?></h2>
     
 </body>
 </html>
