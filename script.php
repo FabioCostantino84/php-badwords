33d8864bbe1e2f6e40969557de8dd1b8f3062496
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-09 17:37:27 +0200

    add $_POST censored

--- script.php
+++ script.php
@@ -1,6 +1,8 @@
 <?php
 
 $frase = $_POST["frase"];
+$censored = $_POST["censored"];
+
 
 
 ?>
@@ -22,6 +24,8 @@ $frase = $_POST["frase"];
         <h2>La tua frase:</h2>
         <p><?php echo $frase; ?> <?php echo strlen($frase); ?></p>
 
+        <h2>Parola da censurare:</h2>
+        <p><?php echo $censored; ?> <?php echo strlen($censored); ?></p>
     </div>
 
 </body>
