c2c8d30db6907263a8db5913ae86bffa73930240
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-09 17:23:52 +0200

    add script on index

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
+<?php
+// commento non visibile in html
+
+/* var_dump(explode(' ', $frase ));
+var_dump(explode(' ', $censured )); */
+
+/* var_dump($frase);
+var_dump($censured); */
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 </head>
+
 <body>
 
-<form>
-    <input type="text" name="frase" id="frase" placeholder="scrivi una frase">
-    <input type="text" name="censured" id="censured" placeholder="parola da censurare">
-    <button type="submit">Save</button>
-</form>
+    <!-- <form>
+        <input type="text" name="frase" id="frase" placeholder="scrivi una frase">
+        <input type="text" name="censured" id="censured" placeholder="parola da censurare">
+        <button type="submit">Save</button>
+    </form> -->
 
-<h2>La tua frase: <?php echo $_GET['frase']?></h2>
-<h2>Parola censurata: <?php echo $_GET['censured']?></h2>
-    
+    <form action="script.php" method="post">
+        <input type="text" name="frase" id="frase" placeholder="Scrivi una frase">
+        <input type="text" name="censored" id="censored" placeholder="Scrivi la parola da censurare">
+        <button type="submit">Save</button>
+    </form>
+
+    <!-- <h2>La tua frase: <?php echo $_GET['frase'] ?></h2>
+    <h2>Parola censurata: <?php echo $_GET['censored'] ?></h2> -->
+
 </body>
+
 </html>
